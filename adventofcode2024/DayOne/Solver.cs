1d33805fd1d622ad327bc9f0d5ff846d7b28e6b6
Preston Guillot <prestonguillot@gmail.com>
2024-12-01 13:55:12 -0500

    day one

---
+++ adventofcode2024/DayOne/Solver.cs
@@ -0,0 +1,50 @@
+﻿namespace adventofcode2024.DayOne;
+
+public static partial class Solver
+{
+    private readonly partial struct Input(IEnumerable<int> left, IEnumerable<int> right)
+    {
+        public IReadOnlyList<int> Left { get; } = left.ToList();
+        public IReadOnlyList<int> Right { get; } = right.ToList();
+
+        public static Input ParseFrom(string input)
+        {
+            var lefts = new List<int>();
+            var rights = new List<int>();
+            
+            foreach (var line in input.Split('\n'))
+            {
+                var matches = DigitsRegex().Matches(line);
+                if (matches.Count == 0) continue;
+                if (matches.Count != 2) throw new Exception("something has gone horribly wrong");
+                lefts.Add(int.Parse(matches[0].Value));
+                rights.Add(int.Parse(matches[1].Value));
+            }
+
+            return new Input(lefts, rights);
+        }
+
+        [System.Text.RegularExpressions.GeneratedRegex(@"\d+")]
+        private static partial System.Text.RegularExpressions.Regex DigitsRegex();
+    }
+    
+    public static long SolvePartOne(string input)
+    {
+        var inputs = Input.ParseFrom(input);
+        var lefts = inputs.Left.OrderBy(x => x);
+        var rights = inputs.Right.OrderBy(x => x);
+        return lefts.Zip(rights, (left, right) => int.Abs(left - right)).Sum();
+    }
+
+    public static long SolvePartTwo(string input)
+    {
+        var inputs = Input.ParseFrom(input);
+        var rightCounts = new Dictionary<int, int>();
+
+        foreach (var number in inputs.Right)
+            if (!rightCounts.TryAdd(number, 1))
+                rightCounts[number]++;
+
+        return inputs.Left.Select(x => x * rightCounts.GetValueOrDefault(x, 0)).Sum();
+    }
+}
---
+++ adventofcode2024/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using adventofcode2024.DayOne;
+
+Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartOne(Data.PartOne.testInput));
+Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartOne(Data.PartOne.trialInput));
+Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartTwo(Data.PartTwo.testInput));
+Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartTwo(Data.PartTwo.trialInput));
