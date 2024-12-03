f3554792b851e30dcacb298f2142db48be3d179f
Preston Guillot <prestonguillot@gmail.com>
2024-12-03 09:20:51 -0500

    day 3

---
+++ adventofcode2024/Day3/Solver.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace adventofcode2024.Day3;
+
+public static class Solver
+{
+    public static long SolvePartOne(string input)
+    {
+        return Regex.Matches(input, @"mul\((\d+)\,(\d+)\)")
+                    .Select(match => (long.Parse(match.Groups[1].Value), long.Parse(match.Groups[2].Value)))
+                    .Select(pair => pair.Item1 * pair.Item2)
+                    .Sum();
+    }
+    
+    public static long SolvePartTwo(string input)
+    {
+        var matches = Regex.Matches(input, @"mul\((\d+)\,(\d+)\)|do\(\)|don\'t\(\)");
+        var enabled = true;
+        var total = 0L;
+
+        foreach (Match match in matches)
+        {
+            if(match.Value.StartsWith("mul") && enabled) total += long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value);
+            else if (match.Value == @"don't()") enabled = false;
+            else if (match.Value == @"do()") enabled = true;
+        }
+        
+        return total;
+    }
+}
--- adventofcode2024/Program.cs
+++ adventofcode2024/Program.cs
@@ -5,7 +5,11 @@
 // Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartOne(adventofcode2024.DayOne.Data.PartOne.trialInput));
 // Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartTwo(adventofcode2024.DayOne.Data.PartTwo.testInput));
 // Console.WriteLine(adventofcode2024.DayOne.Solver.SolvePartTwo(adventofcode2024.DayOne.Data.PartTwo.trialInput));
-Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartOne(adventofcode2024.Day2.Data.PartOne.testInput));
-Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartOne(adventofcode2024.Day2.Data.PartOne.trialInput));
-Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartTwo(adventofcode2024.Day2.Data.PartTwo.testInput));
-Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartTwo(adventofcode2024.Day2.Data.PartTwo.trialInput));
+// Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartOne(adventofcode2024.Day2.Data.PartOne.testInput));
+// Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartOne(adventofcode2024.Day2.Data.PartOne.trialInput));
+// Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartTwo(adventofcode2024.Day2.Data.PartTwo.testInput));
+// Console.WriteLine(adventofcode2024.Day2.Solver.SolvePartTwo(adventofcode2024.Day2.Data.PartTwo.trialInput));
+// Console.WriteLine(adventofcode2024.Day3.Solver.SolvePartOne(adventofcode2024.Day3.Data.PartOne.testInput));
+// Console.WriteLine(adventofcode2024.Day3.Solver.SolvePartOne(adventofcode2024.Day3.Data.PartOne.trialInput));
+// Console.WriteLine(adventofcode2024.Day3.Solver.SolvePartTwo(adventofcode2024.Day3.Data.PartTwo.testInput));
+Console.WriteLine(adventofcode2024.Day3.Solver.SolvePartTwo(adventofcode2024.Day3.Data.PartTwo.trialInput));
